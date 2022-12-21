c58139b3112134071ad17e268de3076fa854bd3e
hi2gage <hi2gage.pdx@gmail.com>
2022-12-20 22:41:28 -0700

    Initial Commit

---
+++ Color Tracker/Color_TrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  Color_TrackerApp.swift
+//  Color Tracker
+//
+//  Created by Gage Halverson on 12/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Color_TrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
